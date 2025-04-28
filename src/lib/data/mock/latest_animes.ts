1fdda03d41e9866f357957d52d6ac4d688b14978
anime-kun32 <alabiadedoyinjohn@protonmail.com>
2025-04-28 17:45:28 +0100

    Update latest_animes.ts

--- src/lib/data/mock/latest_animes.ts
+++ src/lib/data/mock/latest_animes.ts
@@ -7,7 +7,7 @@ export const latest_animes = [
         type: "TV",
         aired_from: "2023-04-20T15:38:51.162Z",
         aired_to: "2023-04-20T15:38:51.162Z",
-        cover: "/images/DemonSlayer-cover.avif",
+        cover: "https://aniteams-player-livid.vercel.app/DemonSlayer-cover.avif",
         synopsis: `It is the Taisho Period in Japan. Tanjiro, a kindhearted boy who sells charcoal for a
 				living, finds his family slaughtered by a demon. To make matters worse, his younger sister
 				Nezuko, the sole survivor, has been transformed into a demon herself. Though devastated by this grim reality, Tanjiro resolves to become a “demon slayer” so that he can turn his
